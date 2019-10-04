1efbcc2c6f0f7509c1a3819cf655cbc4dd92cee2
Ric Lavers <rlavers@deloitte.com.au>
2019-10-04 15:27:30 +1000

    refacted to not use JSX

--- src/tasks/one/TaskOne.jsx
+++ src/tasks/one/TaskOne.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ErrorButton from './ErrorButton';
 
-const MySection = ({ title = '', children }) => {
+const MySection = ({ title, description, children }) => {
   return (
     <section className="task-one__section">
       <h2>{title}</h2>
+      <p>{description}</p>
 
       {children}
     </section>
@@ -28,8 +29,49 @@ export default class TaskOne extends React.Component {
     if (this.state.hasError) {
       return <div>oh no a error occured</div>;
     }
-    return (
-      <MySection title="My title">
+    return React.createElement(
+      MySection,
+      {
+        title: 'Task one ',
+        description: 'refactor this code to use Error Boundaries and NO JSX'
+      },
+      React.createElement(
+        'details',
+        {},
+        React.createElement('summary', {}, 'Fragments'),
+        React.createElement(
+          'p',
+          {},
+          'Let you group a list of children without adding extra nodes to the DOM'
+        )
+      ),
+      React.createElement(
+        'details',
+        {},
+        React.createElement('summary', {}, 'Error boundaries'),
+        React.createElement(
+          'p',
+          {},
+          'Protects your app by catching errors allowing you to render fallback UI or perform side effet such as logging.',
+          React.createElement(ErrorButton)
+        )
+      ),
+      React.createElement(
+        'details',
+        {},
+        React.createElement('summary', {}, 'JSX'),
+        React.createElement(
+          'p',
+          {},
+          'Let you write React.js with the look and feel of HTML'
+        )
+      )
+    );
+  }
+}
+
+/* 
+<MySection title="My title">
         <details>
           <summary>Fragments</summary>
           <p>
@@ -45,7 +87,6 @@ export default class TaskOne extends React.Component {
             <ErrorButton />
           </p>
         </details>
-      </MySection>
-    );
-  }
-}
+			</MySection>
+			
+			*/
