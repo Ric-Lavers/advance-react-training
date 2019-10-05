b0a89b99080f8382f76b757a11003c4567dd020b
Ric Lavers <rlavers@deloitte.com.au>
2019-10-05 12:40:48 +1000

    error button

--- src/tasks/one/ErrorButton.jsx
+++ src/tasks/one/ErrorButton.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 const ErrorButton = () => {
   const [count, setCount] = React.useState(1);
 
-  // const throwError = () => {
-  //   throw new Error('Oopsy theres a little buggy wuggy');
-  // };
-  if (count === 2) {
+  const throwError = () => {
     throw new Error('Oopsy theres a little buggy wuggy');
-  }
-  return (
-    <button onClick={() => setCount(count + 1)}>errors such as this</button>
-  );
+  };
+  // if (count === 2) {
+  //   throw new Error('Oopsy theres a little buggy wuggy');
+  // }
+  return <button onClick={() => throwError()}>errors such as this???</button>;
 };
 
 export default ErrorButton;
